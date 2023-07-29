1563cc74e69aaf2b2f97848a3c6662f4f8b21bcd
Yiim <ilyimyan@gmail.com>
2023-07-29 20:22:07 +0800

    menambahkan hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,22 @@
 module.exports = {
   content: ["./src/public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
     extend: {
       colors : {
         blue : '#4682A9',
         dark : '#334155'
       },
+      screens: {
+        '2xl': '1320px',
+      }
     },
     fontFamily: {
       myFont: 'Ubuntu'
-    }
+    },
   },
   plugins: [],
 }
