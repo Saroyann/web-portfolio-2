1ea2c1790094e073447d78d7912920ba81b331d4
Yiim <ilyimyan@gmail.com>
2023-07-30 13:02:55 +0800

    initial commit

--- src/script/script.js
+++ src/script/script.js
@@ -4,3 +4,9 @@ const hamburger = document.querySelector("#hamburger");
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("hamburger-active");
 })
+
+// navbar fixed 
+window.onscroll = function() {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
+}
