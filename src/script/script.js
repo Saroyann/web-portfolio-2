9e3ec811f77e10e8d97897c9cc8c0d7419e3eda0
Yiim <ilyimyan@gmail.com>
2023-07-31 10:23:13 +0800

    initial commit

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,6 @@
 // hamburger
 const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("hamburger-active");
